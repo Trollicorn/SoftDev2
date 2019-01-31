ba47edfd22712430c77ccd5e40bde8ba4cc76113
Mohammed Uddin <muddin3@stuy.edu>
2019-01-31 01:16:21 -0500

    finished basics for 00

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -1,5 +1,11 @@
 const canvas = document.getElementById("slate");
 const ctx = canvas.getContext("2d");
+var r = canvas.getBoundingClientRect();
+var x = r["x"];
+var y = r["y"];
+var dot = 0;
+var w = 50;
+var h = 100;
 
 var clear = function(e){
 	ctx.clearRect(0,0,canvas.width,canvas.height);
@@ -8,7 +14,26 @@ var clear = function(e){
 var button = document.getElementById("clear");
 button.addEventListener('click',clear);
 
+var toggle = function(e){
+	if (dot == 0){
+		w = 1;
+		h = 1;
+		dot = 1;
+	}
+	else{
+		w = 50;
+		h = 100;
+		dot = 0;
+	}
+};
+
+var toggler = document.getElementById("switch");
+toggler.addEventListener('click',toggle);
+
+
 var draw = function(e){
 	console.log("(" + e.clientX + "," + e.clientY + ")");
-}
+	ctx.fillRect(e.clientX - x, e.clientY - y, w,h);
+};
+
 canvas.addEventListener('click',draw);
