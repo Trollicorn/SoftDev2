c06d6a5b92f0c4391fd0ea4ba9792f19c08e2912
Mohammed Uddin <muddin3@stuy.edu>
2019-05-01 21:40:51 -0400

    add work23

--- 23_closure/ok.py
+++ 23_closure/ok.py
@@ -13,32 +13,20 @@ def greet2():
     greetings = ['print("Hello")','print "Hello"','System.out.println("Hello")','console.log("Hello")']
     return random.choice(greetings)
 
-
-
 #print(greet())
 #print(greet2())
 
 def memoize(f):
-    memo = {}
+    memo = {0:0,1:1,2:1}
     def helper(x):
-        if x in memo:
-            return memo[x]
-        else:
-
+        if x not in memo:
+            memo[x]=f(x)
+        return memo[x]
     return helper
 @memoize
 def fibrec(n):
-    return 1 if n == 0 or n == 1 else fibrec(n-1)+fibrec(n-2)
+    return fibrec(n-1)+fibrec(n-2)
 
 #print(fibrec(40))
 fib = fibrec
-
 print(fib(40))
-
-def fib():
-    nums = [1,1]
-    def inner():
-        nonlocal nums
-        num[1] = num[0]+num[1]
-        num[0] = num[1]
-    return inner
